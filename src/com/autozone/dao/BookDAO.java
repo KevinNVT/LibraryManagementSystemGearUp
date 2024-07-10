822a7c7de6f2484ceef09d9c6c8997a8fad453cd
Kevin Varela <kevinvarelatalamantes@gmail.com>
2024-07-10 14:46:22 -0600

    Fourth commit. Fixed some classes and added more validations

---
+++ src/com/autozone/dao/BookDAO.java
@@ -0,0 +1,206 @@
+package com.autozone.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.autozone.database.DatabaseConnection;
+import com.autozone.models.Book;
+import com.autozone.utils.Validator;
+
+public class BookDAO {
+
+	public void addBook(Book book) throws SQLException, IllegalArgumentException, IllegalAccessException {
+		Validator.validate(book);
+		book.setAvailable(true);
+		
+		String sql = "INSERT INTO tbl_books (title, author, isbn, available) VALUES (?,?,?,?)";
+		
+		//try-with-resources
+		try(Connection conn = DatabaseConnection.getInstance().getConnection();
+				PreparedStatement psmt = conn.prepareStatement(sql)) {
+			
+			psmt.setString(1, book.getTitle());
+			psmt.setString(2, book.getAuthor());
+			psmt.setString(3, book.getIsbn());
+			psmt.setBoolean(4, book.isAvailable());
+
+			psmt.executeUpdate();
+		}	
+	}
+	
+	public void updateBook(Book book) throws SQLException, IllegalArgumentException, IllegalAccessException {
+		Validator.validate(book);
+		
+		String sql = "UPDATE tbl_books SET title = ?, author = ?, isbn = ?, available = ? WHERE id = ?";
+		
+		//try-with-resources
+		try(Connection conn = DatabaseConnection.getInstance().getConnection();
+				PreparedStatement psmt = conn.prepareStatement(sql)) {
+			
+			psmt.setString(1, book.getTitle());
+			psmt.setString(2, book.getAuthor());
+			psmt.setString(3, book.getIsbn());
+			psmt.setBoolean(4, book.isAvailable());
+			psmt.setInt(5, book.getId());
+			
+			psmt.executeUpdate();
+		}
+	}
+	
+	
+	public void deleteBook(int id) throws SQLException {
+		String sql = "DELETE FROM tbl_books WHERE id = ?";
+
+		//try-with-resources
+		try(Connection conn = DatabaseConnection.getInstance().getConnection();
+				PreparedStatement psmt = conn.prepareStatement(sql)) {
+			
+			psmt.setInt(1, id);	
+			psmt.executeUpdate();
+		}	
+	}
+	
+	public List<Book> findAll() throws SQLException {
+		String sql = "SELECT * FROM tbl_books";
+		List<Book> books = new ArrayList<Book>();
+
+		//try-with-resources
+		try(Connection conn = DatabaseConnection.getInstance().getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+				
+			Book book = null;
+			
+			while(rs.next()) {
+				book = new Book(
+						rs.getString("title"), 
+						rs.getString("author"), 
+						rs.getString("isbn"), 
+						rs.getBoolean("available")
+				);
+
+				book.setId(rs.getInt("id"));
+				books.add(book);
+			}
+		}	
+		return books;
+	}
+	
+    public Book findBookById(int id) throws SQLException {
+        String sql = "SELECT * FROM tbl_books WHERE id = ?";
+        Book book = null;
+
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, id);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    book = new Book(
+                            rs.getString("title"),
+                            rs.getString("author"),
+                            rs.getString("isbn"),
+                            rs.getBoolean("available")
+                    );
+                    book.setId(rs.getInt("id"));
+                } else {
+                	System.out.println("Book with ID " + id + " not found.");
+                }
+            }
+        }
+
+        return book;
+    }
+
+	public List<Book> findByTitle(String title) throws SQLException {
+	    String sql = "SELECT * FROM tbl_books WHERE title LIKE ?";
+	    /*I instantiated the list without explicitly specifying the type of elements it will contain. 
+	    This is a test for educational purposes*/
+	    List<Book> books = new ArrayList<>();
+
+	    // try-with-resources
+	    try (Connection conn = DatabaseConnection.getInstance().getConnection();
+	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	        
+	        pstmt.setString(1, "%" + title + "%");
+	        
+	        try (ResultSet rs = pstmt.executeQuery()) {
+	            while (rs.next()) {
+	                Book book = new Book(
+	                    rs.getString("title"),
+	                    rs.getString("author"),
+	                    rs.getString("isbn"),
+	                    rs.getBoolean("available")
+	                );
+	                
+	                book.setId(rs.getInt("id"));
+	                books.add(book);
+	            }
+	        }
+	    }
+	    return books;
+	}
+	
+	public List<Book> findByAuthor(String author) throws SQLException {
+	    String sql = "SELECT * FROM tbl_books WHERE author LIKE ?";
+	    /*I instantiated the list without explicitly specifying the type of elements it will contain. 
+	    This is a test for educational purposes*/
+	    List<Book> books = new ArrayList<>();
+
+	    // try-with-resources
+	    try (Connection conn = DatabaseConnection.getInstance().getConnection();
+	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	        
+	        pstmt.setString(1, "%" + author + "%");
+	        
+	        try (ResultSet rs = pstmt.executeQuery()) {
+	            while (rs.next()) {
+	                Book book = new Book(
+	                    rs.getString("title"),
+	                    rs.getString("author"),
+	                    rs.getString("isbn"),
+	                    rs.getBoolean("available")
+	                );
+	                
+	                book.setId(rs.getInt("id"));
+	                books.add(book);
+	            }
+	        }
+	    }
+	    return books;
+	}
+	
+	public List<Book> findByIsbn(String isbn) throws SQLException {
+		
+		String sql = "SELECT * FROM tbl_books WHERE isbn = ?";
+	    List<Book> books = new ArrayList<>();
+
+	    // try-with-resources
+	    try (Connection conn = DatabaseConnection.getInstance().getConnection();
+	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	        
+	    	pstmt.setString(1, isbn);
+	        
+	        try (ResultSet rs = pstmt.executeQuery()) {
+	            while (rs.next()) {
+	                Book book = new Book(
+	                    rs.getString("title"),
+	                    rs.getString("author"),
+	                    rs.getString("isbn"),
+	                    rs.getBoolean("available")
+	                );
+	                
+	                book.setId(rs.getInt("id"));
+	                books.add(book);
+	            }
+	        }
+	    }
+	    return books;
+	}
+}
---
+++ src/com/autozone/dao/MemberDAO.java
@@ -0,0 +1,155 @@
+package com.autozone.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.autozone.database.DatabaseConnection;
+import com.autozone.models.Member;
+import com.autozone.utils.Validator;
+
+public class MemberDAO {
+
+	public void addMember(Member member) throws SQLException, IllegalArgumentException, IllegalAccessException {
+		Validator.validate(member);
+		
+		String sql = "INSERT INTO tbl_members (member_name, member_id) VALUES (?,?)";
+		
+		//try-with-resources
+		try(Connection conn = DatabaseConnection.getInstance().getConnection();
+				PreparedStatement psmt = conn.prepareStatement(sql)) {
+			
+			psmt.setString(1, member.getMember_name());
+			psmt.setString(2, member.getMember_id());
+			
+			psmt.executeUpdate();
+		}	
+	}	
+	
+	public void updateMember(Member member) throws SQLException, IllegalArgumentException, IllegalAccessException {
+		Validator.validate(member);
+		
+		String sql = "UPDATE tbl_members SET member_name = ?, member_id = ?";
+		
+		//try-with-resources
+		try(Connection conn = DatabaseConnection.getInstance().getConnection();
+				PreparedStatement psmt = conn.prepareStatement(sql)) {
+			
+			psmt.setString(1, member.getMember_name());
+			psmt.setString(2, member.getMember_id());
+			
+			psmt.executeUpdate();
+		}
+	}
+	
+	public void deleteMember(int id) throws SQLException, IllegalArgumentException, IllegalAccessException {
+		// Validates ID is greater than 0
+	    if (id <= 0) {
+	        throw new IllegalArgumentException("ID must be greater than 0.");
+	    }
+
+	    // Verifies the member exists
+	    MemberDAO memberDAO = new MemberDAO();
+	    List<Member> members = memberDAO.findById(String.valueOf(id));
+	    if (members.isEmpty()) {
+	        throw new IllegalArgumentException("Member with ID " + id + " does not exist.");
+	    }
+
+	    // Deletes the member
+	    String sql = "DELETE FROM tbl_members WHERE member_id = ?";
+	    try (Connection conn = DatabaseConnection.getInstance().getConnection();
+	         PreparedStatement psmt = conn.prepareStatement(sql)) {
+
+	        psmt.setInt(1, id);
+	        int affectedRows = psmt.executeUpdate();
+	        
+	        // Validates a member has been eliminated
+	        if (affectedRows == 0) {
+	            throw new SQLException("Failed to delete member. No rows affected.");
+	        }
+	    }		
+	}
+	
+	public List<Member> findAll() throws SQLException {
+		String sql = "SELECT * FROM tbl_members";
+		
+		List<Member> members = new ArrayList<Member>();
+	    if (members.isEmpty()) {
+	        throw new IllegalArgumentException("No members");
+	    }
+
+		//try-with-resources
+		try(Connection conn = DatabaseConnection.getInstance().getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+				
+			Member member = null;
+			
+			while(rs.next()) {
+				member = new Member(rs.getString("member_name"), rs.getString("member_id"));
+
+				member.setId(rs.getInt("id"));
+				members.add(member);
+			}
+		}	
+		return members;
+	}
+	
+	public List<Member> findByName(String member_name) throws SQLException, IllegalArgumentException, IllegalAccessException {
+	    Validator.validate(member_name);
+		
+		String sql = "SELECT * FROM tbl_members WHERE member_name LIKE ?";
+
+	    List<Member> members = new ArrayList<>();
+	    if (members.isEmpty()) {
+	        throw new IllegalArgumentException("Member with name " + member_name + " does not exist.");
+	    }
+
+	    // try-with-resources
+	    try (Connection conn = DatabaseConnection.getInstance().getConnection();
+	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	        
+	        pstmt.setString(1, "%" + member_name + "%");
+	        
+	        try (ResultSet rs = pstmt.executeQuery()) {
+	            while (rs.next()) {
+	                Member member = new Member(rs.getString("member_name"), rs.getString("member_id"));
+	                
+	                member.setId(rs.getInt("id"));
+	                members.add(member);
+	            }
+	        }
+	    }
+	    return members;
+	}
+	
+	public List<Member> findById(String member_id) throws SQLException {
+	    String sql = "SELECT * FROM tbl_members WHERE member_id LIKE ?";
+
+	    List<Member> members = new ArrayList<>();
+	    if (members.isEmpty()) {
+	        throw new IllegalArgumentException("Member with ID " + member_id + " does not exist.");
+	    }
+
+	    // try-with-resources
+	    try (Connection conn = DatabaseConnection.getInstance().getConnection();
+	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	        
+	        pstmt.setString(1, "%" + member_id + "%");
+	        
+	        try (ResultSet rs = pstmt.executeQuery()) {
+	            while (rs.next()) {
+	                Member member = new Member(rs.getString("member_name"), rs.getString("member_id"));
+	                
+	                member.setId(rs.getInt("id"));
+	                members.add(member);
+	            }
+	        }
+	    }
+	    return members;
+	}
+}
